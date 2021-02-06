75296c68e877130a8f328ead3a043a5649560d5a
jeremy <jeremy96kr@naver.com>
2021-02-06 22:37:51 +0900

    PJW addMovie

--- PJW/js/star.js
+++ PJW/js/star.js
@@ -1,7 +1,17 @@
 $('.starRev span').click(function(){
     $(this).parent().children('span').removeClass('on');
     $(this).addClass('on').prevAll('span').addClass('on');
+   
+  
+
     return false;
   });
 
+
+  $('.star7').click(function(){
+   alert("2")
+   
   
+
+    return false;
+  });
